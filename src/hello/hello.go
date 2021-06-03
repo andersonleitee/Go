9ec5bf18b160a017bda6c681d9ac7f959adcb2e1
Anderson Leite <andersondias@cc.ci.ufpb.br>
2021-06-03 02:47:53 -0300

    Entrada de dados

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -5,10 +5,17 @@ import (
 )
 
 func main() {
-	//não precisa atribuir a palavra var para variavel basta apenas :=
 	nome := "Anderson"
 	versao := 1.1
-	idade := 24
-	fmt.Println("Olá, sr.", nome, "sua idade é", idade)
+	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar monitoramento")
+	fmt.Println("2- Receber os Logs")
+	fmt.Println("0- Sair do monitoramento")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println(comando)
+
 }
