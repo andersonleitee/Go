529ea008d8e19b748a9898728657cea02c9a08f7
Anderson Leite <andersondias@cc.ci.ufpb.br>
2021-06-03 02:20:24 -0300

    declaracao de variaveis

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("olá mundo!")
+	var nome = "Anderson" //não precisa dizer o tipo pq o GO faz uma inferência
+	var versao float32 = 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("O tipo da varíavel nome é:", reflect.TypeOf(nome))
+
 }
