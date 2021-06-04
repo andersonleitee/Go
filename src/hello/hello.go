50e033592c8fee95be3592e1294f5a92f5de5499
Anderson Leite <andersondias@cc.ci.ufpb.br>
2021-06-03 23:01:12 -0300

    Funcoes

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,33 +2,45 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	nome := "Anderson"
-	versao := 1.1
-	fmt.Println("Olá, sr.", nome)
-	fmt.Println("Este programa está na versão", versao)
-
-	fmt.Println("1- Iniciar monitoramento")
-	fmt.Println("2- Receber os Logs")
-	fmt.Println("0- Sair do monitoramento")
-
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi ", comando)
+	exibeIntroducao()
+	exibeMenu()
+	comando := comandoLido()
 
 	switch comando {
 	case 0:
 		fmt.Println("Sair do programa")
+		os.Exit(0) //indicar ao SO para sair com seguranca
 	case 1:
 		fmt.Println("Monitorando...")
 	case 2:
 		fmt.Println("Exibindo Logs...")
 	default:
 		fmt.Println("Não conheço este comando")
+		os.Exit(-1) //indicar ao SO que foi um erro n esperado
 	}
 
-	//Não precisa usar o break em Go
+}
 
+func exibeIntroducao() {
+	nome := "Anderson"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar monitoramento")
+	fmt.Println("2- Receber os Logs")
+	fmt.Println("0- Sair do monitoramento")
+}
+
+func comandoLido() int { //caso seja uma funcao que retorne algum dado, seu tipo se declara após declaracao da func
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi ", comandoLido)
+	return comandoLido
 }
